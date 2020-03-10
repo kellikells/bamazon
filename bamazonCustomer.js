19b287bb11959ed03cf39ad4c59881ab44916c09
kelli <kellikells@gmail.com>
2020-03-09 17:53:01 -0700

    created a query to display the id, name, & price from the product TABLE in the bamazonDB

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,9 +3,9 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 
 
-// ===== sets up the ability to connect to DB =======
-//   put into our client(node), 
-//   how to connect to DB
+// ========= Sets up the ability to connect to DB ==========
+//     put into our client(node), how to connect to DB
+// ----------------------------------------------------------
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -14,13 +14,31 @@ const connection = mysql.createConnection({
     database: 'bamazonDB'
 });
 
-// ======== CONNECTING TO DB ============
-//    also tells what to do when connected
+// =================== CONNECTING TO DB =====================
+//         also tells what to do when connected
+// ------------------------------------------------------------
 connection.connect((err) => {
     if (err) throw err;
 
     // thread ID: can be used to kill the thread or if connection is lost, to reconnect
-    console.log("Connected as thread ID: " + connection.threadID);
-    connection.end();
-
+    console.log('Connected as thread ID: ' + connection.threadId);
+    productDisplay();
 });
+
+// ===== Query data: id, names, prices (for display) =======
+// -----------------------------------------------------------
+function productDisplay() {
+    connection.query('SELECT * FROM products', (err, res) => {
+        if (err) throw err;
+
+        // --- loop through response & logs desired 
+        for (let i = 0; i < res.length; i++) {
+            console.log(`id: ${res[i].item_id} | product name: ${res[i].product_name} | price: $ ${res[i].price}`);     //FORMAT(price, 2)
+        }
+        connection.end();  
+    });
+}
+
+
+
+
